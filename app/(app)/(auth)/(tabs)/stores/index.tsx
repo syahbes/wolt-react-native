82e7bb394104eb63e6e4b2415a59cc53505e9594
Shlomo Y <shlomo@techtio.io>
2025-12-04 16:43:14 +0200

    stores

--- app/(app)/(auth)/(tabs)/stores/index.tsx
+++ app/(app)/(auth)/(tabs)/stores/index.tsx
@@ -1,9 +1,36 @@
+import { CategoryList } from '@/components/CategoryList';
+import RestaurantHeader from '@/components/RestaurantHeader';
+import RestaurantList from '@/components/RestaurantList';
+import { Fonts } from '@/constants/theme';
 import { StyleSheet, Text, View } from 'react-native';
+import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const RestaurantListPage = () => {
+const HEADER_HEIGHT = 60;
+
+const ResturantListPage = () => {
+  const insets = useSafeAreaInsets();
+  const scrollOffset = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollOffset.value = event.contentOffset.y;
+    },
+  });
   return (
     <View style={styles.container}>
-      <Text>Stores</Text>
+    <RestaurantHeader title='Stores' scrollOffset={scrollOffset}/>
+      <Animated.ScrollView
+        showsVerticalScrollIndicator={false}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
+        contentContainerStyle={{ paddingTop: insets.top + HEADER_HEIGHT }}
+      >
+        <Text style={styles.pageTitle}>Stores</Text>
+        <CategoryList />
+        <Text style={styles.allRestaurantsTitle}>All Stores</Text>
+        <RestaurantList />
+      </Animated.ScrollView>
     </View>
   );
 };
@@ -12,6 +39,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  pageTitle: {
+    fontFamily: Fonts.brandBlack,
+    fontSize: 30,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+  allRestaurantsTitle: {
+    fontFamily: Fonts.brandBold,
+    fontSize: 20,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
 });
-
-export default RestaurantListPage;
+export default ResturantListPage;
